a3b387d292c8f4eb5c8964e77b3397fcb20362fb
D2SIADO <d2siado@gmail.com>
2022-08-17 19:35:17 -0600

    Added role perm to the commands :rat:

--- bot/events/interaction.js
+++ bot/events/interaction.js
@@ -1,4 +1,6 @@
 const client = require("../../index");
+const { MessageEmbed } = require('discord.js')
+const config = require('../../config/config')
 const chalk = require('chalk')
 
 client.on('interactionCreate', async(interaction) => {
@@ -7,12 +9,40 @@ client.on('interactionCreate', async(interaction) => {
     const commands = client.commands.get(interaction.commandName)
   
     if (!commands) return
-  
+
     if (commands.permission) {
       const authorPerms = interaction.channel.permissionsFor(interaction.member)
-      if(!authorPerms || !authorPerms.has(commands.permission)) {
-        return interaction.reply( { content: `${"'❌'"} You need the perm ${commands.permission} to execute the command`})
-      }
+      if(!authorPerms || !authorPerms.has(commands.permission)) return interaction.reply({
+        embeds: [ new MessageEmbed()
+            .setAuthor({
+                name: interaction.user.tag,
+                iconURL: interaction.user.avatarURL()
+            }).setTitle('No permissions').setColor('RED')
+            .setDescription(`${"`❌`"} You need the permission '${commands.permission}' to execute the command`)
+            .setFooter({
+                text: config.name,
+                iconURL: config.bot.icon
+            })
+            .setImage(`https://i.ibb.co/QjxPpsZ/Red.png`)
+            .setTimestamp()]
+    , ephemeral: true})
+    }
+    if(commands.role) {
+      const member = interaction.member
+      if (!member.roles.cache.has(commands.role)) return interaction.reply({
+        embeds: [ new MessageEmbed()
+            .setAuthor({
+                name: interaction.user.tag,
+                iconURL: interaction.user.avatarURL()
+            }).setTitle('No permissions').setColor('RED')
+            .setDescription(`${"`❌`"} You need the role <@&${commands.role}> to execute the command`)
+            .setFooter({
+                text: config.name,
+                iconURL: config.bot.icon
+            })
+            .setImage(`https://i.ibb.co/QjxPpsZ/Red.png`)
+            .setTimestamp()]
+    , ephemeral: true})
     }
   
     try {
